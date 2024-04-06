9584112dd352a3d70208210d96d8f088639fc8b7
kaizu-r <jljoloc@gmail.com>
2024-04-06 23:31:31 +0800

    commit branch

--- logbooktest.c
+++ logbooktest.c
@@ -5,7 +5,8 @@
 typedef struct logbook{
     char plateNum[MAX];
     char profileID[MAX];
-    char type;
+    //unsure!!
+    //char type;
     struct tm timeIn;
     struct tm timeOut;
     struct logbook * next;
@@ -19,7 +20,7 @@ void useLog(log *head, int option);
 //please define with return int (0 or 1)
 void traverseProfile(void);
 //please define with void return and void parameters
-void register(void);
+void registerProfile(void);
 ///----------------------------------------------------------==
 
 
@@ -35,12 +36,13 @@ int main()
 //option if log in (1) or log out (2)
 void useLog(log *head, int option)
 {
-    log * p, *q;
+    log * p, *q, *new;
     char tempNo[MAX];
     char tempID[MAX];
     int choice;
     p = head;
     q = head;
+    time_t t;
     if(option == 0)
     {
         printf("Log IN\nPlate No: ");
@@ -52,9 +54,26 @@ void useLog(log *head, int option)
         {
             printf("Plate Number is not in our data base.\n\t1. End Transaction\n\t2. Register Profile\n");
             scanf("%d", &choice);
-            //register profile
-
+            if(option == 1)
+                return;
+            else if(option == 2)
+                registerProfile();
         }
+        //adding log details
+        while(p->next != NULL)
+        {
+            p = p->next;
+            if(p->next == NULL)
+            {
+                new = (log *) malloc(sizeof(log));
+                
+                strcpy(new->plateNum, tempNo);
+                strcpy(new->profileID, tempID);
+                
+
+            }
+        }
+
 
     }
 
