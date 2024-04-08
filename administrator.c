07ab8f1e53ffd13d6de37230e431c51e5c801601
OverwatchXX <xarisdelarosat@gmail.com>
2024-04-08 10:32:33 +0800

    create_list function & changes on administrator.c syntax

--- administrator.c
+++ administrator.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<string.h>
 #include <time.h>
 #define MAX 100
 
@@ -17,15 +18,19 @@ struct node { //struct of Profile
 };// for records linked list
 typedef struct node Profile;
 
-/*void traverseProfile(void){}
-void dscrpncyCheck(void){}*/
+/*FUNCTION PROTOTYPES*/
 
-/*int Administrator(Profile * head){
+/*void traverseProfile(void);
+void dscrpncyCheck(void){}*/
+Profile *create_list(FILE*inrec);//read profile records from file
+void display(Profile*head);//to display data from records file on screen(temp function)
+void Administrator(Profile *head);
+
+void Administrator(Profile *head) {
     Admin admin;
-    Profile * p, *new;
+    Profile *p = head, *new;
     int option;
     char plate[MAX];
-    p = head;
 
     printf("Admin: ");
     scanf("%s", admin.user);
@@ -35,65 +40,65 @@ void dscrpncyCheck(void){}*/
     printf("\n1. PMS\n2. Search Profile\n");
     scanf("%d", &option);
 
-    if(option == 1){ //PMS instructions
+    if (option == 1) { // PMS instructions
         printf("\nChoose Action:\n1. Park In\n2. Park Out\n");
         scanf("%d", &option);
-        if(option == 1){ //if Park In
-            traverseProfile();
+        if (option == 1) { // if Park In
+            // Call function for Park In
+        } else if (option == 2) { // if Park Out
+            // Call function for Park Out
         }
-        else if(option == 2){ //if Park Out
-            dscrpncyCheck();
-        }
-    }
-    else if(option == 2){ //Search Profile Instructions
+    } else if (option == 2) { // Search Profile Instructions
         printf("Enter Plate Number: ");
         scanf("%s", plate);
-        while(1){ //Traverse the linked list
-            if (p->nxtPtr == NULL){ //If plate number not found
-                printf("\nPlate number not found\n");
-                printf("\n1. End Transaction\n2. Register Profile\n");
-                scanf("%d", &option);
-                if(option == 1){
-                    break;
-                }
-                else if(option == 2){ //Profile register
-                    new = (Profile *) malloc(sizeof(Profile));
-                    printf("REGISTER");
-                    Printf("Profile ID: ");
-                    scanf("%s", new->profileID);
-                    Printf("Vehicle Type: ");
-                    scanf(" %c", new->type);
-                    strcpy(new->plateNum, plate);
-                    p->nxtPtr=new;
-                    new->nxtPtr=NULL;
-                    break;
-                }
-            }
-            else if(plate == (*p).plateNum){ //If plate number found
-                printf("%s %s", (*p).plateNum, (*p).profileID);
+        while (p != NULL) { // Traverse the linked list
+            if (strcmp(p->plateNum, plate) == 0) { // If plate number found
+                printf("%s %s\n", p->plateNum, p->profileID);
                 break;
             }
-            else{ //Continue to look for the plate number
-                p = p->nxtPtr;
+            p = p->nxtPtr;
+        }
+        if (p == NULL) { // If plate number not found
+            printf("Plate number not found\n");
+            printf("\n1. End Transaction\n2. Register Profile\n");
+            scanf("%d", &option);
+            if (option == 2) { // Profile register
+                new = (Profile *) malloc(sizeof(Profile));
+                printf("REGISTER\n");
+                printf("Profile ID: ");
+                scanf("%s", new->profileID);
+                printf("Vehicle Type: ");
+                scanf(" %c", &new->type);
+                strcpy(new->plateNum, plate);
+
+                // Traverse to the end of the list to add new profile
+                p = head;
+                while (p->nxtPtr != NULL) {
+                    p = p->nxtPtr;
+                }
+                p->nxtPtr = new;
+                new->nxtPtr = NULL;
             }
         }
     }
-}*/
+}
+
 
 int main(){
 
-    Profile profile;
+    Profile *profile;
     
-    FILE *inrec = fopen("C:\\Users\\xaris\\OneDrive\\Documents\\C Files\\records.txt", "r");
-    FILE *inlog = fopen("C:\\Users\\xaris\\OneDrive\\Documents\\C Files\\logbook.txt", "r");
-    FILE *indcy = fopen("C:\\Users\\xaris\\OneDrive\\Documents\\C Files\\discrepancy.txt", "r");
+    FILE *inrec = fopen("records.txt", "r");
+    /*FILE *inlog = fopen("logbook.txt", "r");
+    FILE *indcy = fopen("discrepancy.txt", "r");*/
 
-    if (inrec == NULL || inlog == NULL || indcy == NULL) {
+    if (inrec == NULL /*|| inlog == NULL || indcy == NULL*/) {
        printf("Error opening files.\n");
        return 1; // Exit with error
     }
    
-    printf("hi Xar!");
+    profile = create_list(inrec); 
+    display(profile);
     //CODE GOES HERE - xar;)
     //Administrator(&profile);
     
@@ -101,6 +106,38 @@ int main(){
 
 
     fclose(inrec);
-    fclose(inlog);
-    fclose(indcy);
+    /*fclose(inlog);
+    fclose(indcy);*/
 }
+
+Profile *create_list(FILE*inrec){
+  int k,n;
+  Profile *p, *head;
+  fscanf(inrec, "%d", &n);//num of profiles in the records
+  for(k=0; k<n; k++){
+    if(k==0){
+      head=(Profile*)malloc(sizeof(Profile));
+      p=head;
+    }
+    else{
+      p->nxtPtr=(Profile*)malloc(sizeof(Profile));
+      p=p->nxtPtr;
+    }
+    fscanf(inrec,"%s %s %c", p->plateNum, p->profileID, &p->type);
+  }
+  p->nxtPtr=NULL;
+  return head;
+}//creating profile linked list function
+
+void display(Profile*head){
+  int count = 1;
+  Profile *p;
+  p=head;
+  printf("Data in Records File:");
+  while(p!=NULL){
+    printf("\n%s %s %c", p->plateNum, p->profileID, p->type);
+    count++;
+    p=p->nxtPtr;
+  }
+  printf("\n");
+}//temporary funcion for checking
