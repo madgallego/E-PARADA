e636db61354c88a9dbeef1710e277679a4fec7e9
kaizu-r <jljoloc@gmail.com>
2024-04-21 16:52:01 +0800

    added new currLog function

--- printLog.c
+++ printLog.c
@@ -18,9 +18,11 @@ typedef struct logbook{
     struct logbook * next;
 }log;
 
-//prints logbook to logbook.txt. Happens at the end of program execution
-void printLog(log * head);
+//prints logbook. Option 0 for print to screen(search logs for today), 1 for print to logbook.txt(end of execution)
+void printLog(log * head, int option);
 
+//prints current logged in users. Happens after every termination of useLog function(to update logbook displayed)
+void currLog(log * head);
 int main()
 {
     //ensure that head is never lost
@@ -41,7 +43,7 @@ int main()
     printLog(head);
 }
 
-void printLog(log * head)
+void printLog(log * head, int option)
 {
     log * p = head;
     FILE * ptr = fopen("logboook.txt", "a");
@@ -54,21 +56,53 @@ void printLog(log * head)
     {
         //gets date of timeIn
         strftime(timeIN, sizeof(timeIN), "%x", &p->timeIn);
-
-        printf("| %s | ", timeIN);
-        fprintf(ptr, "| %s | ", timeIN);
-        printf("%s | %s ", p->plateNum, p->profileID);
-        fprintf(ptr, "%s | %s ", p->plateNum, p->profileID);
-        //gets hour of time in
-        //DI KO ALAM BAKIT PULA YUNG FORMAT SPECIFIER PERO NAGA WORK YAN I TESTED IT
-        strftime(timeIN, sizeof(timeIN), "%I:%M%p", &p->timeIn);
-        strftime(timeOUT, sizeof(timeOUT), "%I:%M%p", &p->timeOut);
-
-        printf("| %s | %s |\n", timeIN, timeOUT);
-        fprintf(ptr, "| %s | %s |\n", timeIN, timeOUT);
+        //runs if option is 1
+        if(option)
+        {
+            fprintf(ptr, "| %s | ", timeIN);
+            fprintf(ptr, "%s | %s ", p->plateNum, p->profileID);
+            //gets hour of time in
+            //DI KO ALAM BAKIT PULA YUNG FORMAT SPECIFIER PERO NAGA WORK YAN I TESTED IT
+            strftime(timeIN, sizeof(timeIN), "%I:%M%p", &p->timeIn);
+            strftime(timeOUT, sizeof(timeOUT), "%I:%M%p", &p->timeOut);
+            fprintf(ptr, "| %s | %7s |\n", timeIN, timeOUT);
+        }
+        else
+        {
+            printf("| %s | ", timeIN);
+            printf("%s | %s ", p->plateNum, p->profileID);
+            //gets hour of time in
+            //DI KO ALAM BAKIT PULA YUNG FORMAT SPECIFIER PERO NAGA WORK YAN I TESTED IT
+            strftime(timeIN, sizeof(timeIN), "%I:%M%p", &p->timeIn);
+            strftime(timeOUT, sizeof(timeOUT), "%I:%M%p", &p->timeOut);
+            printf("| %s | %7s |\n", timeIN, timeOUT);
+        }
 
         p = p->next;
     }
     fclose(ptr);
+    return;
+}
+
+
+void currLog(log * head)
+{
+    log * p = head;
+    char timeIN[100];
+    char timeOUT[100];
+    printf("CURRENT LOGGED IN USERS\n");
+    while(p->next != NULL)
+    {
+        if(p->status != 0)
+        {
+            //gets date of timeIn
+            strftime(timeIN, sizeof(timeIN), "%x", &p->timeIn);
+            printf("| %s | ", timeIN);
+            printf("%s | %s |\n", p->plateNum, p->profileID);
+            printf("\n");
+        }
+
+        p = p->next;
+    }
     return;
 }
