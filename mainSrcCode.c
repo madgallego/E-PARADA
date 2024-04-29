2302c68605a7af44c2371ce18c2cadea476cca05
OverwatchXX <xarisdelarosat@gmail.com>
2024-04-30 05:16:14 +0800

    fixed useLog at mainSrcCode

--- mainSrcCode.c
+++ mainSrcCode.c
@@ -104,7 +104,7 @@ void dscrpncyCheck(Profile * profile, char plate[], char id[]);
 int useLog(log *head, Profile * pHead, int * car, int * motor, int option);
 
 //creates new struct for new profile and store to a.txt file
-void registerProfile(void);
+//void registerProfile(void);
 
 //Main function admin log in and for managing the program
 void Administrator(Profile **head);
@@ -389,28 +389,31 @@ void dscrpncyCheck(Profile * head, char plate[], char id[]){
 //option if log in (1) or log out (2)
 int useLog(log *head, Profile * pHead, int * car, int * motor, int option)
 {
-    log * p, *new;
+    log *p, *new;
     char tempNo[MAX];
     char tempID[MAX];
-    int choice, temphr, tempmin, outNotFound = 0;
+    int choice;
+    int temphr, tempmin;
+    int outNotFound = 0;
+    time_t t;
 
     p = head;
-    time_t t;
-    if(option == 1)
-    {
+
+    if(option == 1){
         printf("Log IN\nPlate No: ");
         scanf("%s", tempNo);
         printf("Driver ID: ");
         scanf("%s", tempID);
         //ends once profile is registered or transaction is ended
-        if(traverseProfile(pHead, tempNo) == 1)
-        {
+        if(traverseProfile(pHead, tempNo) == 1){
             printf("Plate Number is not in our data base.\n\t1. End Transaction\n\t2. Register Profile\n");
             scanf("%d", &choice);
-            if(option == 1)
+            if(option == 1){
                 return 0;
-            else if(option == 2)
+            }
+            else if(option == 2){
                 registerProfile(/*parameters here*/);
+            }
         }
         dscrpncyCheck(pHead, tempNo, tempID);
         //making new pointer
@@ -420,120 +423,74 @@ int useLog(log *head, Profile * pHead, int * car, int * motor, int option)
         t = time(NULL);
         new->timeIn = *localtime(&t);
         new->next = NULL;
-        if(traverseProfile(pHead, tempNo) == 2)
-        {
-            for(int i = 0; i < 20; i++)
-            {
-                //free spot
-                if(car[i] == 0)
-                {
-                    //setup parking spot for return
-                    new->status = i + 1;
+
+        int profileType = traverseProfile(pHead, tempNo);
+
+        if (profileType == 2) {
+            for (int i = 0; i < 20; i++) {
+                if (car[i] == 0) {
+                    new->status = i + 1; // setup parking spot for return
                     car[i] = 1;
                     break;
                 }
             }
-        }
-        else if(traverseProfile(pHead, tempNo) == 3)
-        {
-            for(int i = 0; i < 20; i++)
-            {
-                //free spot
-                if(motor[i] == 0)
-                {
-                    //setup parking spot for return
-                    new->status = i + 21;
+        } else if (profileType == 3) {
+            for (int i = 0; i < 20; i++) {
+                if (motor[i] == 0) {
+                    new->status = i + 21; // setup parking spot for return
                     motor[i] = 1;
                     break;
                 }
             }
         }
-    
         //adding log details
-        while(p->next!= NULL)
-        {
+        while(p->next!= NULL){
             p = p->next;
         }
-        if(p->next == NULL)
-        {
-            *p = *new;
-            p->next = (log *) malloc(sizeof(log));
-            p->next->next = NULL;
-            //RETURNS PARKING SPOT. 1 IS LOWEST. RETURN 0 IS LOG OUT 
-            return p->status;
-        }
+        
+        p->next = new;
+
+        // Return parking spot number, 0 for logout
+        return new->status;
     }
-    else if(option == 2)
-    {
-        do
-        {
+     else if (option == 2) {
+        do {
             printf("Log OUT\nPlate No: ");
             scanf("%s", tempNo);
             printf("Driver ID: ");
             scanf("%s", tempID);
 
-            //CHECKS IF ALREADY LOGGED OUT
-
-            if((strcmp(p->plateNum, tempNo) == 0) && p->status == 0)
-            {
-                printf("User already logged out. Exiting Log Out.\n");
-                return 0;
-            }
-            while((strcmp(p->plateNum, tempNo) != 0) && p->next->next != NULL)
-            {
-                p = p->next;
-                if((strcmp(p->plateNum, tempNo) == 0) && p->status == 0)
-                {
-                    printf("User already logged out. Exiting Log Out.\n");
-                    return 0;
-                }
-            }
-
-            //resets p back to head
+            // Reset pointer to the start of the list
             p = head;
 
-            //CHECK FOR DISCREPANCY
-            dscrpncyCheck(pHead, tempNo, tempID);
-            //find platenumber to log out
-            if((strcmp(p->plateNum, tempNo) == 0) && p->status != 0)
-            {             
-                t = time(NULL);
-                p->timeOut = *localtime(&t);
-                p->status = 0;
+            while (p != NULL) {
+                if (strcmp(p->plateNum, tempNo) == 0) {
+                    if (p->status == 0) {
+                        printf("User already logged out. Exiting Log Out.\n");
+                        return 0;
+                    } else {
+                        dscrpncyCheck(pHead, tempNo, tempID);
 
-                //calculating balance
-                printf("Total balance is: Php %.2f", (((p->timeOut.tm_hour*60 + p->timeOut.tm_min)) - (p->timeIn.tm_hour*60 + p->timeIn.tm_min)) * 0.5 /*rate*/);
+                        t = time(NULL);
+                        p->timeOut = *localtime(&t);
+                        p->status = 0;
 
-                return p->status;
-                    
-            }
-            while((strcmp(p->plateNum, tempNo) != 0) && p->next->next != NULL)
-            {
-                p = p->next;
-                //driver found and checks if driver is logged out or not
-                if((strcmp(p->plateNum, tempNo) == 0) && p->status != 0)
-                {             
-                    t = time(NULL);
-                    p->timeOut = *localtime(&t);
-                    //change timeOut to admin input (comment if not necessary)
-                    p->timeOut.tm_hour = temphr;
-                    p->timeOut.tm_min = tempmin;
-                    p->status = 0;
+                        float balance = (((p->timeOut.tm_hour * 60 + p->timeOut.tm_min) - (p->timeIn.tm_hour * 60 + p->timeIn.tm_min)) * 0.5);
+                        printf("Total balance is: Php %.2f\n", balance);
 
-                    //calculating balance
-                    printf("Total balance is: Php %.2f", (((temphr*60 + tempmin)/60) - (p->timeIn.tm_hour*60 + p->timeIn.tm_min)) * 0.5 /*rate*/);
-
-                    return p->status;
-                    
+                        return p->status;
+                    }
                 }
+
+                p = p->next;
             }
-            printf("Driver not logged in. \n1. End Transaction\n2. Log Out another driver\n");
+
+            printf("Driver not logged in.\n\t1. End Transaction\n\t2. Log Out another driver\n");
             scanf("%d", &choice);
-            if(choice == 1)
-                return 0;
-            else if(choice == 2)
-                ;//DO NOTHING
-        //Checks if car was even logged in (prevent security breaches)
-        } while(1);
+        } while (choice == 2);
+
+        return 0;
     }
+
+    return -1; // Invalid option
 }
