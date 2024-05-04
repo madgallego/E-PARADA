5a50644fb1f94b9f7447ac863a97311005e0f8bc
kaizu-r <jljoloc@gmail.com>
2024-05-04 12:46:39 +0800

    added archive feature of records.txt

--- printLog.c
+++ printLog.c
@@ -18,35 +18,34 @@ typedef struct logbook{
     struct logbook * next;
 }log;
 
+struct node { //struct of Profile
+	char plateNum [MAX];
+	char profileID [MAX];
+	char type;
+    struct node * nxtPtr;
+};// for records linked list
+typedef struct node Profile;
+
+
 //prints logbook. Option 0 for print to screen(search logs for today), 1 for print to logbook.txt(end of execution)
-void printLog(log * head, int option);
+void printLog(log * head, int option, FILE * ptr);
 
 //prints current logged in users. Happens after every termination of useLog function(to update logbook displayed)
 void currLog(log * head);
+//prints profile back to records.txt for updating. Called at the end of program before freeProfile
+void archiveProf(Profile * head);
+
 int main()
 {
     //ensure that head is never lost
-    log * head = (log*) malloc(sizeof(log));
-    head->next = NULL;
-    log *q, * p = head;
+    log * head = NULL;
     //delete last log since it containts garbage
-    while(p->next != NULL)
-    {
-        q = p;
-        p = p->next;
-    }
-    if(p->next == NULL)
-    {
-        free(p);
-        q->next = NULL;
-    }
     printLog(head);
 }
 
-void printLog(log * head, int option)
+void printLog(log * head, int option, FILE * ptr)
 {
     log * p = head;
-    FILE * ptr = fopen("logboook.txt", "a");
 
     char timeIN[100];
     char timeOUT[100];
@@ -80,7 +79,7 @@ void printLog(log * head, int option)
 
         p = p->next;
     }
-    fclose(ptr);
+
     return;
 }
 
@@ -120,5 +119,20 @@ void currLog(log * head)
 
         p = p->next;
     }
+    return;
+}
+
+void archiveProf(Profile * head)
+{
+    //reopen records.txt for rewriting (saving profiles)
+    FILE * rec = fopen("records.txt", "w");
+    Profile * p = head;
+    while(p != NULL)
+    {
+        fprintf(rec, "%s %s %c\n", p->plateNum, p->profileID, p->type);
+        p = p->next;
+    }
+
+    fclose(rec);
     return;
 }
